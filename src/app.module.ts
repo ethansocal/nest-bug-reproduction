0270a4ae75ee01c10a106c03ebdb81b840b346c1
Ethan <ehtanhnery@gmail.com>
2023-07-11 14:19:54 -0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,5 @@
-import { Module } from "@nestjs/common"
-import { FencingTimeModule } from "./fencingtime/fencingtime.module"
-import { ScheduleModule } from "@nestjs/schedule"
-import { RedisModule } from "nestjs-redis"
-
 @Module({
     imports: [
-        FencingTimeModule,
-        ScheduleModule.forRoot(),
-        RedisModule.register({
-            url: "redis://localhost:6379",
-        }),
     ],
 })
 export class AppModule {}
